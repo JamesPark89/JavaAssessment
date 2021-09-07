6c976cb5cd0e2fd90ebaec06c8b7a3d205b76310
JamesPark89 <87226158+JamesPark89@users.noreply.github.com>
2021-09-07 22:35:07 +1000

    2021.09.07
    -add favourite, contact page
    -adjust some layout

--- index.js
+++ index.js
@@ -5,12 +5,28 @@ const ingredients = document.getElementById('ingredients');
 const searchText = document.getElementById('searchText');
 const searchButton = document.getElementById('searchButton');
 const result = document.getElementById('result');
+const contents = document.getElementById('contents');
 
 searchButton.addEventListener("click", getData);
+
 //fetching API data function
 async function getData(){
-  removeAllChildNodes(result);
   const search = searchText.value;
+
+  //reload the contents page
+  removeAllChildNodes(contents);
+  var newResult = document.createElement('DIV');
+  newResult.className = 'result';
+  contents.appendChild(newResult);
+
+  var newText = document.createElement('DIV');
+  var x = document.createTextNode(`This the result of searching by ${search}`);
+  newText.appendChild(x);
+  newText.className = 'newSearchText';
+  newResult.appendChild(newText);
+  
+  //fetching the data
+
   var data = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${search}&app_id=2b0ebcea&app_key=c49b82c2800324bd3157d984848f9aa1`)
   data = await data.json()
   for(i=0;i < data.hits.length;i++){
@@ -18,6 +34,8 @@ async function getData(){
   var dishImg = data.hits[i].recipe.image
   var dishIngredients = data.hits[i].recipe.ingredientLines;
   console.log(data.hits);
+
+  //show the searching result
 
 
   var newDiv = document.createElement('DIV');
@@ -28,20 +46,23 @@ async function getData(){
   img.className = 'card-img-top';
   newDiv.appendChild(img);
 
+  var newSection = document.createElement('DIV');
+  newSection.className = 'section';
+  newDiv.appendChild(newSection);
+
   var newH5 = document.createElement('H5');
-  var x = document.createTextNode(dishName);
-  newH5.appendChild(x); 
-  newDiv.appendChild(newH5);
+  var y = document.createTextNode(dishName);
+  newH5.appendChild(y); 
+  newSection.appendChild(newH5);
   
   var divIng = document.createElement('DIV');
-  var y = document.createTextNode(dishIngredients);
-  divIng.appendChild(y); 
-  newDiv.appendChild(divIng);
-  
-  result.appendChild(newDiv);
+  var z = document.createTextNode(dishIngredients);
+  divIng.appendChild(z); 
+  newSection.appendChild(divIng);
+  newResult.appendChild(newDiv);
+}
 
-
-}}
+}
 
 // Execute a function when the user releases a Enter key on the keyboard
 searchText.addEventListener("keydown", function(event) {
